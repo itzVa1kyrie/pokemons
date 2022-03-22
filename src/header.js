a8acfaeeebc127d5b311dbdb8873aa78b5c5669c
itzVa1kyrie <irongalaxy6@gmail.com>
2022-03-22 16:39:41 +0200

    Refinement
    Splitting files that used axios
    Using hooks instead of classes
    Getting rid of direct access to the document
    Using the clsx library

--- src/header.js
+++ src/header.js
@@ -1,23 +1,5 @@
 import React, {useEffect, useRef} from 'react';
 
-<<<<<<< Updated upstream
-export default class Header extends Component {
-    render() {
-        return (
-            <header className="header">
-                <div className="container header_flex-wrapper">
-                    <a href="/">
-                        <img className="header-flex-wrapper_img" src="images/logo.png"/>
-                    </a>
-                    <div className="header-flex-wrapper_search">
-                        <p className="search_hint">Enter the name or number of the pokemon</p>
-                        <div>
-                            <input className="search_inp" placeholder="Search your pokemon..."/>
-                            <button className="search_btn" onClick={() => this.props.onClick()}>
-                                <img className="search-btn_img" src="images/search_white.png"/>
-                            </button>
-                        </div>
-=======
 export default function Header(props) {
     const btn = useRef(null);
     const inp = useRef(null);
@@ -60,7 +42,6 @@ export default function Header(props) {
                         <button ref={btn} className="search_btn" onClick={searchPokemon}>
                             <img className="search-btn_img" src="images/search_white.png" alt=""/>
                         </button>
->>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
